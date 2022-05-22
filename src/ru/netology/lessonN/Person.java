4da8bd0a5d36853c4ae8ba393eb38f22f3176adc
MalinovskaNA <delenmir@yandex.ru>
2022-05-22 11:30:35 +0300

    home work pattern builder final

--- src/ru/netology/lessonN/Person.java
+++ src/ru/netology/lessonN/Person.java
@@ -3,24 +3,24 @@ package ru.netology.lessonN;
 import java.util.Objects;
 
 public class Person {
-    protected final String NAME;
-    protected final String SURNAME;
+    protected final String name;
+    protected final String surname;
     private int age;
     private String city;
 
     public Person(String name, String surname, int age, String city) {
-        this.NAME = name;
-        this.SURNAME = surname;
+        this.name = name;
+        this.surname = surname;
         this.age = age;
         this.city = city;
     }
 
     public String getName() {
-        return NAME;
+        return name;
     }
 
     public String getSurname() {
-        return SURNAME;
+        return surname;
     }
 
     public int getAge() {
@@ -52,20 +52,20 @@ public class Person {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Person person = (Person) o;
-        return age == person.age && NAME.equals(person.NAME) && SURNAME.equals(person.SURNAME) && city.equals(person.city);
+        return age == person.age && name.equals(person.name) && surname.equals(person.surname) && city.equals(person.city);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(NAME, SURNAME, age, city);
+        return Objects.hash(name, surname, age, city);
     }
 
     @Override
     public String toString() {
         StringBuilder builder = new StringBuilder();
         builder.append("Person [name=")
-                .append(NAME).append(", surname=")
-                .append(SURNAME).append(", age=")
+                .append(name).append(", surname=")
+                .append(surname).append(", age=")
                 .append(age).append(", city=")
                 .append(city).append("]");
         return builder.toString();
@@ -73,9 +73,9 @@ public class Person {
 
     public PersonBuilder newChildBuilder() {
         PersonBuilder person = new PersonBuilder();
-        person.setName(NAME)
+        person.setName(name)
                 //setName(name != null && !name.trim().isEmpty()?getName():name)
-                .setSurname(SURNAME)
+                .setSurname(surname)
                 .setAddress(city).build();
         return person;
     }
